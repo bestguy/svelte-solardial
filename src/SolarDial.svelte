9816e59074171eb6c024d40ab9b93d55bc0e4137
bestguy <gary@gary.local>
2021-01-03 09:31:52 -0800

    Update SolarDial.svelte

--- src/SolarDial.svelte
+++ src/SolarDial.svelte
@@ -6,14 +6,12 @@
   import Clock from './Clock.svelte';
   
   const range = n => [...Array(n).keys()];
-  
-  let interval;
+
   export let lat = 0;
   export let lon = 0;
+
+  let interval;
   let now = new Date();
-  let sunrise = new Date();
-  let sunset = new Date();
-  let midnight = new Date();
   let dayLength = 1;
   let sunriseAngle = 0;
   let sunsetAngle = 0;
@@ -29,12 +27,10 @@
   $: hourAngle = (now.getHours() + now.getMinutes() / 60) * 15 + 90;
   $: {
     const times = SunCalc.getTimes(now, lat, lon);
-    sunrise = times.sunrise;
-    sunset = times.sunset;
-    midnight = startOfDay(now);
+    const midnight = startOfDay(now);
     dayLength = differenceInSeconds(endOfDay(now), midnight);
-    sunriseAngle = differenceInSeconds(midnight, sunrise) / dayLength * 360;
-    sunsetAngle = differenceInSeconds(midnight, sunset) / dayLength * 360;
+    sunriseAngle = differenceInSeconds(midnight,  times.sunrise) / dayLength * 360;
+    sunsetAngle = differenceInSeconds(midnight, times.sunset) / dayLength * 360;
     solarNoonAngle = differenceInSeconds(midnight, times.solarNoon) / dayLength * 360;
     nadirAngle = differenceInSeconds(midnight, times.nadir) / dayLength * 360;
     night = differenceInSeconds(midnight, times.night) / dayLength * 360;
@@ -109,60 +105,40 @@
     </g>
     {#each range(144) as minute}
       <line
-        x1={93}
-        y1={0}
-        x2={97}
-        y2={0}
+        x1={93} y1={0} x2={97} y2={0}
         opacity={.5}
         stroke="currentColor"
         transform={`rotate(${minute * 2.5})`} />
     {/each}
     {#each range(24) as hour}
+    <g transform={`rotate(${hour * 15})`}>
+      <line x1={90} y1={0} x2={97} y2={0} stroke="currentColor" />
       {#if !(hour % 2)}
-        <g transform={`rotate(${hour * 15})`}>
-          <line
-            x1={90}
-            y1={0}
-            x2={97}
-            y2={0}
-            stroke="currentColor" />
-          <text
-            x={0}
-            y={85}
-            fill="currentColor"
-            font-family="sans-serif"
-            font-size={10}
-            alignment-baseline="middle"
-            text-anchor="middle">
-            {hour}
-          </text>
-        </g>
-      {/if}
+        <text
+          x={0}
+          y={85}
+          fill="currentColor"
+          font-family="sans-serif"
+          font-size={10}
+          alignment-baseline="middle"
+          text-anchor="middle">
+          {hour}
+        </text>
+        {/if}
+      </g>
     {/each}
     
     <g transform={`rotate(${hourAngle})`}>
       <line
-        x1={0}
-        y1={0}
-        x2={61}
-        y2={0}
-        stroke="currentColor"
-        stroke-linecap="round"
-        stroke-width={2} />
+        x1={0} y1={0} x2={61} y2={0}
+        stroke="currentColor" stroke-linecap="round" stroke-width={2} />
       <line
-        x1={73}
-        y1={0}
-        x2={97}
-        y2={0}
-        stroke="currentColor"
-        stroke-linecap="round"
-        stroke-width={2} />
+        x1={73} y1={0} x2={97} y2={0}
+        stroke="currentColor" stroke-linecap="round" stroke-width={2} />
       <circle
         cx={67} cy={0} r={6}
-        stroke="currentColor"
-        stroke-width={2}
-        fill="currentColor"
-        fill-opacity={.5} />
+        stroke="currentColor" stroke-width={2}
+        fill="currentColor" fill-opacity={.5} />
     </g>
     <g transform={`translate(-50, -50) translate(${-30 * Math.cos(hourAngle * Math.PI / 180)},${-30 * Math.sin(hourAngle * Math.PI / 180)})`}>
       <Clock time={now} />
